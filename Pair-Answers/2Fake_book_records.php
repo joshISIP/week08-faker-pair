c6808e0b3f6b8f2d06b73fdaf22bbc2ed4cd139e
joshISIP <2020800065@psu.palawan.edu.ph>
2025-02-27 15:57:43 +0800

    Inserted the question above the code in a comment format

--- Pair-Answers/2Fake_book_records.php
+++ Pair-Answers/2Fake_book_records.php
@@ -1,4 +1,26 @@
 <?php
+/*
+Create a PHP Script to populate a Books Table using Faker.
+Each book should have the following details:
+1. Title
+2. Author
+3. Genre
+4. Publication Year
+5. ISBN (Random 13-digit number ISBN)
+6. Summary
+Predefined Genres: 
+1. Fiction
+2. Mystery
+3. Thriller
+4. Romance
+5. Science Fiction
+6. Fantasy
+7. Horror
+8. Historical
+*/
+
+require 'vendor/autoload.php';
+
 
 echo '
 <!DOCTYPE html>
