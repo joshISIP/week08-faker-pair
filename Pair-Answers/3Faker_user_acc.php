6a5985bdb81ec38e112b59332eeeb9d520bb5660
joshISIP <2020800065@psu.palawan.edu.ph>
2025-02-27 16:15:43 +0800

    finalized the html  side of the code

--- Pair-Answers/3Faker_user_acc.php
+++ Pair-Answers/3Faker_user_acc.php
@@ -20,20 +20,34 @@ $uuid = $faker->uuid;
 $fullname = $faker->name;
 $email = $faker->email;
 $username = explode('@', $email)[0];
-$password = hash('sha256', $faker->password);
+$password = $faker->sha256($password);
 $account_created = $faker->dateTimeBetween($startDate = '-2 years', $endDate = 'now')->format('Y-m-d H:i:s');
 echo'
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Fake User Accounts with System</title>
+     <title>Fake User Accounts</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    
-</body>
-</html>
-';
+    <body>
+    <h1 class="text-center">Fake Book Records</h1>
+    <table border="1" align="center">
+        <thead>
+            <tr>
+                <th>User ID</th>
+                <th>Full Name</th>
+                <th>Email Address</th>
+                <th>User Name</th>
+                <th>Password</th>
+                <th>Date Account Created</th>
+            </tr>
+        </thead>
+        <tbody>';
+for ($i = 0; $i < 10; $i++){
+
+}
 ?>
